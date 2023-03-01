fc7ffaf38b9e4c066ab8ca1123232c5641633144
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-03-01 17:47:20 +0300

    header - feat: add Logo component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,30 @@
 import { type } from "os";
 import React, {Component} from "react";
 import "./header.css";
+import logoImage from "../../images/svg/tree.svg";
+
+
+type LogoState = {
+    active: boolean;
+    logoImage: string;
+};
+
+class Logo extends Component<{}, LogoState> {
+
+    state: LogoState = {
+        active: false,
+        logoImage: logoImage
+    }
+
+    render(): React.ReactNode {
+        const logo = this.state.logoImage;
+        return (
+            <div className="logo">
+                <img src={logo} alt="logo-image"/>
+            </div>
+        );
+    }
+}
 
 
 
@@ -13,12 +37,15 @@ const HeaderButton = ({text}: HeaderButtonProps) => {
     return (<button>{text}</button>);
 };
 
+
+
 export default class Header extends Component {
 
     render(): React.ReactNode {
 
         return (
         <header className="header">
+            <Logo/>
             <HeaderButton text="ИГРУШКИ"/>
             <HeaderButton text="ЕЛКА"/>
         </header>);

cf7868f0bc32defe12af0a1bb50c686a9dcbbbf6
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-03-02 17:35:01 +0300

    main - feat: add background image

--- src/components/start-page/start-page.tsx
+++ src/components/start-page/start-page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./start-page.css";
+import backgroundImg from "../../images/bg.jpg";
 
 const StartPage = () => {
-    return (<div className="start-page">Start page</div>);
+    return (<div className="start-page" style={{ 
+        backgroundImage: `url(${backgroundImg})` 
+      }}>Start page</div>);
 };
 
 export default StartPage;
